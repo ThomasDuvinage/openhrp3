fe3444d78d8e40d51528fe2a7676aba83f1fdff4
ThomasDuvinage <toto.duvinage@gmail.com>
2025-04-06 19:48:44 +0900

    [sample] update BasicDataTypeSkel.h include position

--- sample/controller/PD_HGtest/PD_HGtest.h
+++ sample/controller/PD_HGtest/PD_HGtest.h
@@ -19,13 +19,13 @@
 #ifndef PD_HGtest_H
 #define PD_HGtest_H
 
+#include <rtm/idl/BasicDataTypeSkel.h>
 #include <rtm/idl/BasicDataType.hh>
 #include <rtm/Manager.h>
 #include <rtm/DataFlowComponentBase.h>
 #include <rtm/CorbaPort.h>
 #include <rtm/DataInPort.h>
 #include <rtm/DataOutPort.h>
-#include <rtm/idl/BasicDataTypeSkel.h>
 #include <rtm/idl/ExtendedDataTypesSkel.h>
 
 #include <vector>
